3d51802adc9595e08f7f10eba655d97ffaa018e9
AMEY PARUNDEKAR <AMEY PARUNDEKAR>
2017-04-27 13:49:17 +0530

    Sides_created

--- lily.py
+++ lily.py
@@ -6,21 +6,38 @@ canvas_height = 500
 def paint(event):
     python_green = "#476042"
     x, y = event.x,event.y
-    x1, y1 = (event.x-1), (event.y-1)
-    x2, y2 = (event.x+1), (event.y-1)
-    w.create_line(0,0,x1,y1,fill="blue")
-    #w.create_line(0,500,x1,y1,fill="red")
-    #w.create_line(500,0,x1,y1,fill="yellow")
-    #w.create_line(500,500,x1,y1,fill="green")
-    slopePerpendicular1= -x/y
-    slopePerpendicular2= (500-x)/y
-    slopePerpendicular3=  x/500-y
-    slopePerpendicular4=  -(500-x)/(500-y) 
+    x1, y1 = (event.x-3), (event.y-3)
+    x2, y2 = (event.x+3), (event.y+3)
+    w.create_rectangle(x1,y1,x2,y2,fill="red")
     
+#define slopes    
+    slopePerpendicular1 = -x/y
+    slopePerpendicular2 = (500-x)/y
+    slopePerpendicular3 = (x-500)/(500-y)
+    slopePerpendicular4 = x/(500-y)
+
+#define boundary points    
     y3 = y/2 + slopePerpendicular1*(-x/2)
     x3 = ((-y/2)/slopePerpendicular1)+x/2
-    w.create_line(x3,0,0,y3,fill="green")
 
+    x4 = - y/(2*slopePerpendicular2) + (500+x)/2
+    y4 = y/2 + slopePerpendicular2*(500 - (500+x)/2)
+
+    x5 = (250+0.5*y)/slopePerpendicular3 + 250 + x
+    y5 = (500+y)/2 + slopePerpendicular3*(250 - x/2)
+
+    x6 = (250 - 0.5*y)/slopePerpendicular4 + x/2
+    y6 = (500+y)/2 + slopePerpendicular4*(-x/2)
+
+#find intersection
+    
+#create lines
+    w.create_line(x3,0,0,y3,fill="white")
+    w.create_line(x4,0,500,y4,fill="white")
+    w.create_line(x5,500,500,y5,fill="white")
+    w.create_line(x6,500,0,y6,fill = "white")
+
+#define canvas   
 master = Tk()
 master.title("Points")
 w = Canvas(master,
@@ -32,4 +49,6 @@ message = Label(master, text="Click and choose a point")
 message.pack(side=BOTTOM)
 
 mainloop()
+
+
 
